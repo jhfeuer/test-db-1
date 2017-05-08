8193e99720e8e4ea4703f78ebfda2fb869e13c76
Jacob Feuerborn <jhfeuer@umich.edu>
2017-05-08 19:39:28 +0000

    added some simple (very simple) styling

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,7 @@
 class Record < ApplicationRecord
+    validates :serialNum, presence: true
+    validates :product, presence: true
+    validates :removalDate, presence: true
+    validates :supplier, presence: true
+    validates :partNum, presence: true
 end
